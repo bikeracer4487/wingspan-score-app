008ce6a1764604adc1068a799fe3586928c79571
Doug Mason <github@dougmason.tech>
2026-01-01 10:37:06 -0500

    Update bird avatars and configuration
    - Removed outdated bird avatar images and added new ones, including Bald Eagle, Barn Owl, and Canada Goose.
    - Updated the bird avatar constants to reflect the new images and improved naming conventions.
    - Cleaned up the project configuration by removing unnecessary XML declarations.

--- src/constants/birdAvatars.ts
+++ src/constants/birdAvatars.ts
@@ -1,8 +1,5 @@
 /**
  * Bird avatar images for player profiles.
- *
- * Note: These are placeholder images from Lorem Picsum.
- * For production, replace with actual bird photographs or illustrations.
  */
 
 export interface BirdAvatar {
@@ -12,36 +9,36 @@ export interface BirdAvatar {
 }
 
 export const BIRD_AVATARS: BirdAvatar[] = [
-  { id: 'cardinal', name: 'Northern Cardinal', image: require('../../assets/bird-avatars/cardinal.jpg') },
+  { id: 'baldeagle', name: 'Bald Eagle', image: require('../../assets/bird-avatars/baldeagle.jpg') },
+  { id: 'barnowl', name: 'Barn Owl', image: require('../../assets/bird-avatars/barnowl.jpg') },
   { id: 'bluejay', name: 'Blue Jay', image: require('../../assets/bird-avatars/bluejay.jpg') },
-  { id: 'robin', name: 'American Robin', image: require('../../assets/bird-avatars/robin.jpg') },
-  { id: 'oriole', name: 'Baltimore Oriole', image: require('../../assets/bird-avatars/oriole.jpg') },
-  { id: 'goldfinch', name: 'American Goldfinch', image: require('../../assets/bird-avatars/goldfinch.jpg') },
-  { id: 'hummingbird', name: 'Hummingbird', image: require('../../assets/bird-avatars/hummingbird.jpg') },
-  { id: 'owl', name: 'Barn Owl', image: require('../../assets/bird-avatars/owl.jpg') },
-  { id: 'hawk', name: 'Red-tailed Hawk', image: require('../../assets/bird-avatars/hawk.jpg') },
-  { id: 'eagle', name: 'Bald Eagle', image: require('../../assets/bird-avatars/eagle.jpg') },
-  { id: 'falcon', name: 'Peregrine Falcon', image: require('../../assets/bird-avatars/falcon.jpg') },
-  { id: 'duck', name: 'Mallard Duck', image: require('../../assets/bird-avatars/duck.jpg') },
-  { id: 'heron', name: 'Great Blue Heron', image: require('../../assets/bird-avatars/heron.jpg') },
-  { id: 'kingfisher', name: 'Kingfisher', image: require('../../assets/bird-avatars/kingfisher.jpg') },
-  { id: 'woodpecker', name: 'Woodpecker', image: require('../../assets/bird-avatars/woodpecker.jpg') },
+  { id: 'cardinal', name: 'Northern Cardinal', image: require('../../assets/bird-avatars/cardinal.jpg') },
   { id: 'chickadee', name: 'Chickadee', image: require('../../assets/bird-avatars/chickadee.jpg') },
-  { id: 'nuthatch', name: 'Nuthatch', image: require('../../assets/bird-avatars/nuthatch.jpg') },
-  { id: 'wren', name: 'House Wren', image: require('../../assets/bird-avatars/wren.jpg') },
-  { id: 'sparrow', name: 'House Sparrow', image: require('../../assets/bird-avatars/sparrow.jpg') },
+  { id: 'crane', name: 'Crane', image: require('../../assets/bird-avatars/crane.jpg') },
   { id: 'crow', name: 'American Crow', image: require('../../assets/bird-avatars/crow.jpg') },
-  { id: 'raven', name: 'Common Raven', image: require('../../assets/bird-avatars/raven.jpg') },
-  { id: 'mockingbird', name: 'Northern Mockingbird', image: require('../../assets/bird-avatars/mockingbird.jpg') },
-  { id: 'waxwing', name: 'Cedar Waxwing', image: require('../../assets/bird-avatars/waxwing.jpg') },
-  { id: 'tanager', name: 'Scarlet Tanager', image: require('../../assets/bird-avatars/tanager.jpg') },
-  { id: 'finch', name: 'House Finch', image: require('../../assets/bird-avatars/finch.jpg') },
-  { id: 'warbler', name: 'Yellow Warbler', image: require('../../assets/bird-avatars/warbler.jpg') },
-  { id: 'pelican', name: 'Brown Pelican', image: require('../../assets/bird-avatars/pelican.jpg') },
+  { id: 'duck', name: 'Mallard Duck', image: require('../../assets/bird-avatars/duck.jpg') },
+  { id: 'eagle', name: 'Golden Eagle', image: require('../../assets/bird-avatars/eagle.jpg') },
+  { id: 'falcon', name: 'Peregrine Falcon', image: require('../../assets/bird-avatars/falcon.jpg') },
   { id: 'flamingo', name: 'American Flamingo', image: require('../../assets/bird-avatars/flamingo.jpg') },
-  { id: 'parrot', name: 'Parrot', image: require('../../assets/bird-avatars/parrot.jpg') },
+  { id: 'goldfinch', name: 'American Goldfinch', image: require('../../assets/bird-avatars/goldfinch.jpg') },
+  { id: 'goose', name: 'Canada Goose', image: require('../../assets/bird-avatars/goose.jpg') },
+  { id: 'greathornedowl', name: 'Great Horned Owl', image: require('../../assets/bird-avatars/greathornedowl.jpg') },
+  { id: 'heron', name: 'Great Blue Heron', image: require('../../assets/bird-avatars/heron.jpg') },
+  { id: 'hummingbird', name: 'Hummingbird', image: require('../../assets/bird-avatars/hummingbird.jpg') },
+  { id: 'kestrel', name: 'American Kestrel', image: require('../../assets/bird-avatars/kestrel.jpg') },
+  { id: 'kingfisher', name: 'Kingfisher', image: require('../../assets/bird-avatars/kingfisher.jpg') },
+  { id: 'loon', name: 'Common Loon', image: require('../../assets/bird-avatars/loon.jpg') },
+  { id: 'magpie', name: 'Magpie', image: require('../../assets/bird-avatars/magpie.jpg') },
+  { id: 'oriole', name: 'Baltimore Oriole', image: require('../../assets/bird-avatars/oriole.jpg') },
+  { id: 'penguin', name: 'Penguin', image: require('../../assets/bird-avatars/penguin.jpg') },
+  { id: 'puffin', name: 'Atlantic Puffin', image: require('../../assets/bird-avatars/puffin.jpg') },
+  { id: 'robin', name: 'American Robin', image: require('../../assets/bird-avatars/robin.jpg') },
+  { id: 'scarletmacaw', name: 'Scarlet Macaw', image: require('../../assets/bird-avatars/scarletmacaw.jpg') },
+  { id: 'snowyowl', name: 'Snowy Owl', image: require('../../assets/bird-avatars/snowyowl.jpg') },
+  { id: 'stellarsjay', name: "Steller's Jay", image: require('../../assets/bird-avatars/stellarsjay.jpg') },
+  { id: 'swan', name: 'Mute Swan', image: require('../../assets/bird-avatars/swan.jpg') },
   { id: 'toucan', name: 'Toucan', image: require('../../assets/bird-avatars/toucan.jpg') },
-  { id: 'peacock', name: 'Peacock', image: require('../../assets/bird-avatars/peacock.jpg') },
+  { id: 'woodpecker', name: 'Woodpecker', image: require('../../assets/bird-avatars/woodpecker.jpg') },
 ];
 
 /**
